303bfa1438e755ef6633fa9155772a6eb803aa4a
subhiksha2611 <60611731+subhiksha2611@users.noreply.github.com>
2022-09-02 22:10:46 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+        System.out.print("Midday blues");
+        System.out.print("Learn with programming");
+
+	}
+
+}
